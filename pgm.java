cbb39d9a3e795455d753942027c88063f099a7d8
Sadiallahi <ilahisabah13@gmail.com>
2025-11-20 21:10:01 +0530

    something

---
+++ pgm.java
@@ -0,0 +1,7 @@
+public class pgm{
+    public static void main(String[] myArgument){
+        System.out.println("HW");
+        System.out.println("my name is"+myArgument[0]);
+        System.out.println("i am"+myArgument[1]+"yrs old");
+    }
+}
